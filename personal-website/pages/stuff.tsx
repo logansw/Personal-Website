d4e7bc4ec669725adcadf2d0185ff7d95d626372
logansw <logansw@uw.edu>
2021-08-17 17:30:00 -0700

    Stuff page basic scaffolding

--- personal-website/pages/stuff.tsx
+++ personal-website/pages/stuff.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import styles from '../styles/stuff.module.css'
 import NavBar from "../components/NavBar";
+import ProjectPreview from "../components/ProjectPreview";
 
 export default class Stuff extends React.Component {
   render() {
     return(
       <div>
         <NavBar active="stuff"/>
-        <h1>Stuff</h1>
+        <main className={styles.main}>
+          <div className={styles.wrap}>
+            <ProjectPreview img="/Whaleshark Logo 2.svg"></ProjectPreview>
+            <ProjectPreview img="/EasyBites.svg"></ProjectPreview>
+            <ProjectPreview img="/Project Solitaire.svg"></ProjectPreview>
+            <ProjectPreview img="/NaireAtive.svg"></ProjectPreview>
+            <ProjectPreview img="/Hop Vehicle.svg"></ProjectPreview>
+            <ProjectPreview img="/Super Spacebar.svg"></ProjectPreview>
+          </div>
+        </main>
       </div>
     );
   }
