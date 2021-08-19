5f73ce058d4576f8066c1a2c8d11aef41763dd76
logansw <logansw@uw.edu>
2021-08-18 17:29:50 -0700

    ProjectPage and ProjectSection designed and implemented (not responsive yet)

--- personal-website/components/ProjectPage.tsx
+++ personal-website/components/ProjectPage.tsx
@@ -15,21 +15,27 @@ export default class ProjectPage extends React.Component<Props> {
   render() {
     return (
       <div className={styles.projectPage}>
-        <div className={styles.header}>
-          <Image  src={this.props.img}
-                  alt={this.props.title}
-                  width={80}
-                  height={80}/>
-          <div className={styles.titlePair}>
-            <h1>{this.props.title}</h1>
-            <h3>{this.props.date}</h3>
-          </div>
-          <div className={styles.close} onClick={this.props.closePopup}>
-            <Image  src="/x.svg"
-                    alt="Close"
-                    width={24}
-                    height={24}/>
-          </div>
+        <div style={{ backgroundColor: this.props.color }} className={styles.colorSplash}/>
+        <div className={styles.main}>
+          <section className={styles.header}>
+            <Image  src={this.props.img}
+                    alt={this.props.title}
+                    width={80}
+                    height={80}/>
+            <div className={styles.titlePair}>
+              <h1 className={styles.h1}>{this.props.title}</h1>
+              <h3 className={styles.h3}>{this.props.date}</h3>
+            </div>
+            <div className={styles.close} onClick={this.props.closePopup}>
+              <Image  src="/x.svg"
+                      alt="Close"
+                      width={40}
+                      height={40}/>
+            </div>
+          </section>
+          <section className={styles.body}>
+            {this.props.children}
+          </section>
         </div>
       </div>
     );
---
+++ personal-website/components/ProjectSection.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import styles from "./ProjectSection.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+type Size = "h2" | "h3";
+
+interface Props {
+  sectionHeader: string,
+  sectionSize?: Size,
+  callout?: boolean
+}
+
+export default class ProjectSection extends React.Component<Props> {
+  render() {
+    let header;
+    switch (this.props.sectionSize) {
+      case "h2":
+        header = styles.h2;
+        break;
+      case "h3":
+        header = styles.h3;
+        break;
+      default:
+        header = styles.h2;
+    }
+
+    if (this.props.callout) {
+      return (
+        <div className={styles.callout}>
+          <div className={styles.bar}/>
+          <div>
+            <div className={header}>{this.props.sectionHeader}</div>
+            {this.props.children}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <section className={styles.section}>
+          <div className={header}>{this.props.sectionHeader}</div>
+          {this.props.children}
+        </section>
+      );
+    }
+  }
+}
